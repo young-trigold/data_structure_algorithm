1232551f859c723023dbc1d5162123b926d95f8a
young-trigold <cluscandlot63@gmail.com>
2022-01-11 23:23:37 +0800

    update

--- sandbox.js
+++ sandbox.js
@@ -1,3 +1,23 @@
-const threeEmpties = new Array(3,3);
-console.log(threeEmpties);
-// -> [3, 3]
+Array.prototype.insert = function insert(index, ...items) {
+  this.splice(index, 0, ...items);
+  return this;
+};
+
+Array.prototype.update = function update(index, ...items) {
+  this.splice(index, 1, ...items);
+  return this;
+};
+
+Array.prototype.remove = function remove_(index, removeCount) {
+  this.splice(index, removeCount);
+  return this;
+};
+
+const fruits = ['orange', 'watermelon', 'strawberry'];
+fruits
+  .insert(fruits.length, 'apple')
+  .update(fruits.length - 1, 'pear')
+  .remove(fruits.length - 1, 1);
+
+console.log(fruits);
+// -> [ 'orange', 'watermelon', 'strawberry' ]
