aa458d19b27a2631ef63b29ad78b68ef2117eb1e
young-trigold <cluscandlot63@gmail.com>
2022-01-15 18:33:51 +0800

    update

--- sandbox.js
+++ sandbox.js
@@ -1,27 +1,3 @@
-Array.generate = function (len, pattern) {
-  const arr = [];
-
-  for (let i = 0; i < len; i++) {
-    arr[i] = pattern(i);
-  }
-
-  return arr;
-};
-
-Array.prototype.shuffle = function () {
-  const len = this.length;
-
-  for (let i = len - 1; i > 0; i--) {
-    const j = ~~(Math.random() * (i + 1));
-    [this[i], this[j]] = [this[j], this[i]];
-  }
-
-  return this;
-};
-
-const nums = Array.generate(10, (i) => i);
-console.log(...nums);
-// 
-
-const shuffledNums = nums.shuffle();
-console.log(...shuffledNums);
+const fruits = ['apple', 'orange', 'strawberry'];
+console.log(fruits.push('pear'));
+console.log(...fruits);
