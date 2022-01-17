2c608c5d330a5ac3bd0b2c62d2fa6aba84cbe317
young-trigold <cluscandlot63@gmail.com>
2022-01-17 18:43:40 +0800

    upate

--- sandbox.js
+++ sandbox.js
@@ -1,10 +1,53 @@
 class Stack {
-  constructor(){
-   this.cur = 0;
-   this.size = 0;
-   this.items = 0;
+  constructor() {
+    this.size = 0;
   }
 
-  moveTo
+  push(ele) {
+    this[this.size] = ele;
+    this.size++;
+    return this;
+  }
 
+  pop() {
+    const elePoped = this[this.size - 1];
+    delete this[this.size - 1];
+    this.size--;
+    return elePoped;
+  }
+
+  peek() {
+    return this[this.size - 1];
+  }
+
+  clear() {
+    for (let i = 0; i < this.size; i++) {
+      delete this[i];
+    }
+
+    this.size = 0;
+
+    return this;
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
 }
+
+const stack = new Stack();
+stack.push('apple').push('orange').push('pear');
+console.log(stack);
+// -> Stack { '0': 'apple', '1': 'orange', '2': 'pear', size: 3 }
+
+console.log(stack.peek());
+// -> 'pear'
+
+console.log(stack.pop());
+// -> 'pear'
+
+console.log(stack);
+// -> Stack { '0': 'apple', '1': 'orange', size: 2 }
+
+console.log(stack.isEmpty());
+// -> false
