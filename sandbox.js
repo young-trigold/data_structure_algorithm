c09ca1c3afc3134a90f91fe608bf60276ee6420b
young-trigold <cluscandlot63@gmail.com>
2022-01-18 14:14:06 +0800

    update

--- sandbox.js
+++ sandbox.js
@@ -1,56 +1,10 @@
-class Queue {
-  constructor() {
-    this.size = 0;
-  }
-
-  enqueue(ele) {
-    this[this.size] = ele;
-    this.size++;
-    return this;
-  }
-
-  dequeue() {
-    const eleDequeued = this[0];
-    for (let i = 0; i < this.size - 1; i++) {
-      this[i] = this[i + 1];
-    }
-    delete this[this.size-1];
-    this.size--;
-    return eleDequeued;
-  }
-
-  peek() {
-    return this[0];
-  }
-
-  isEmpty() {
-    return this.size === 0;
-  }
-
-  clear() {
-    for (let i = 0; i < this.size; i++) {
-      delete this[i];
-    }
-
-    this.size = 0;
-    return this;
-  }
+if (true) {
+  var a = 0;
 }
 
-const queue = new Queue();
+function foo() {
+  console.log(a);
+}
 
-queue.enqueue('vip').enqueue('member').enqueue('visitor');
-console.log(queue);
-// -> Queue { '0': 'vip', '1': 'member', '2': 'visitor', size: 3 }
-
-console.log(queue.peek());
-// -> 'vip'
-
-console.log(queue.dequeue());
-// -> 'vip'
-
-console.log(queue);
-// -> Queue { '1': 'member', '2': 'visitor', size: 2 }
-
-console.log(queue.clear());
-// -> Queue { size: 0 }
+foo();
+let a = 1;
