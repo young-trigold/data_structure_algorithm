65e3448d2076247ba44ebb5a482ddae16181005d
young-trigold <cluscandlot63@gmail.com>
2022-02-06 15:39:56 +0800

    update

---
+++ doublylinkedlist.js
@@ -0,0 +1,52 @@
+class Node {
+  constructor(element) {
+    this.element = element;
+    this.pre = null;
+    this.next = null;
+  }
+}
+
+const DoublyLinkedList = (() => {
+  const store = new WeakMap();
+
+  class DoublyLinkedList {
+    constructor() {
+      this.head = null;
+      store.set(this, { _count: 0 });
+    }
+
+    get size() {
+      return store.get(this)._count;
+    }
+
+    append(element) {
+      const newNode = new Node(element);
+
+      if (this.isEmpty) {
+        this.head = newNode;
+      } else {
+        let curNode = this.head;
+
+        while (curNode.next) {
+          curNode = curNode.next;
+        }
+
+        curNode.next = newNode;
+      }
+    }
+
+    insert(element, index) {}
+
+    remove(element) {}
+
+    removeAt(index) {}
+
+    indexOf(element) {}
+
+    getNodeAt(index) {}
+
+    [Symbol.iterator]() {}
+  }
+
+  return DoublyLinkedList;
+})();
--- sandbox.js
+++ sandbox.js
@@ -1,19 +1,3 @@
-class MySet extends Set {
-  uninon(set) {
-    if (set instanceof Set) {
-      return new Set([...this, ...set]);
-    }
-  }
+const arr = new Array(Infinity);
 
-  intersect(set) {}
-
-  differ(set) {}
-
-  isSubsetOf() {}
-}
-
-const set = new MySet().add(1).add(2).add(3);
-
-const set2 = new MySet().add(1).add(4).add(7);
-
-console.log(set.uninon(set2));
+console.log(arr);
